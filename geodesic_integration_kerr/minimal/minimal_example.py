1f98e6aac6c0590e6d8fcb8a3ba1a40699d418db
garishka <gergana.daskalova01@gmail.com>
2023-11-29 01:50:14 +0200

    Поправен бъг за неочакван шум в картинката след инплементиране на multiprocessing. Променяне на броя на процесите, които се изпълняват едновременно -> едвам забележимо подобрение във времето. Промяна на стойността на входни параметри. Добавяне на docstring на файла

--- geodesic_integration_kerr/minimal/minimal_example.py
+++ geodesic_integration_kerr/minimal/minimal_example.py
@@ -1,3 +1,46 @@
+"""
+Ray Tracing Simulation for a Kerr Black Hole
+--------------------------------------------
+
+This script simulates the trajectory of light rays around a Kerr Black Hole and produces an image of the observed
+celestial sphere.
+
+Parameters:
+-----------
+- res (int): Image resolution.
+- r0 (float): Initial radial coordinate.
+- th0 (float): Initial polar coordinate.
+- a0 (float): Spin parameter of the black hole.
+- alpha_values (numpy.ndarray): Vertical coordinates of the camera's view.
+- beta_values (numpy.ndarray): Horizontal coordinates of the camera's view.
+- step (float): Step size for the symplectic integrator.
+- omg (float): Coupling constant for the doubled phase space used in the integration scheme.
+- steps (int): Number of integration steps.
+- r_plus (float): Black Hole event horizon.
+
+Functions:
+----------
+- sigma_expr(r, theta, a): Expression for sigma, a metric component.
+- delta_expr(r, a): Expression for delta, a metric component.
+- A_expr(r, theta, a): Expression for A, a metric component.
+- g00(r, th, a), g03(r, th, a), g30(r, th, a), g11(r, th, a), g22(r, th, a), g33(r, th, a): Nonzero covariant metric components.
+- inv_g00(r, th, a), inv_g03(r, th, a), inv_g30(r, th, a), inv_g11(r, th, a), inv_g22(r, th, a), inv_g33(r, th, a):
+Nonzero inverse metric components.
+- hamiltonian(qp, a): Hamiltonian for m=0.
+- init_p(r, th, a, alpha_i, beta_i): Initialize 4-momentum at (-β_i, α_i).
+- solve_ivp(i, j): Solve the initial value problem for the given pixel coordinates (i, j).
+
+Main Execution:
+---------------
+- The script initializes an image with a white background.
+- It uses ray tracing and symplectic integration to determine the trajectory of light rays.
+(- Dual numbers are used in the symplectic integrator scheme to increase accuracy.)
+- Concurrent processing is employed to enhance computational efficiency.
+
+Note: Adjust the parameters and functions as needed for specific scenarios.
+"""
+
+
 import itertools
 import logging
 import numpy as np
@@ -7,18 +50,18 @@ import concurrent.futures
 
 from geodesic_integration_kerr.integrator import symplectic_integrator
 
-res = 14    # image resolution
-r0 = 1_000     # initial radial coordinate
-th0 = np.pi/2   # initial azimuthal coordinate
-a0 = 0.99   # spin param. of the BH
-alpha_values = np.linspace(-5e-3*np.pi, 5e-3*np.pi, res)
-beta_values = np.linspace((1-5e-3)*np.pi, (1+5e-3)*np.pi, res)
+res = 100
+r0 = 1_000
+th0 = np.pi/2
+a0 = 0.99
+alpha_values = np.linspace(-3e-3*np.pi, 3e-3*np.pi, res)
+beta_values = np.linspace((1-3e-3)*np.pi, (1+3e-3)*np.pi, res)
 
-step = 0.2  # step size for the symplectic integrator
-omg = 1     # coupling constant for the doubled phase space, using in the integration scheme
-steps = 10_000  # number of integration steps
+step = 0.2
+omg = 1
+steps = 10_000
 
-r_plus = 1 + np.sqrt(1 - a0 ** 2)   # BH event horizon
+r_plus = 1 + np.sqrt(1 - a0 ** 2)
 
 
 def sigma_expr(r, theta, a):
@@ -69,7 +112,7 @@ def hamiltonian(qp, a):
         inv_g22(r, th, a) * p2 ** 2 + inv_g33(r, th, a) * p3 ** 2)
 
 
-def init_p(r, th, a, alpha_i, beta_i):
+def init_p(r: float, th: float, a: float, alpha_i: float, beta_i: float):
     gamma_g = -g03(r, th, a) / np.sqrt(g33(r, th, a) * (g03(r, th, a) ** 2 - g00(r, th, a) * g33(r, th, a)))
     zeta = np.sqrt(g33(r, th, a) / (g03(r, th, a) ** 2 - g00(r,th, a) * g33(r, th, a)))
 
@@ -94,11 +137,13 @@ def solve_ivp(i, j):
         if results[1, k] <= r_plus + 1e-1:
             # The light ray falls into the black hole; return the values of the pixel that has to be set black
             # С concurrent.futures.ProcessPoolExecutor() не знам как да акумулира промените вместо да създава нова
-            # картинка на всеки 7-8 процеса
+            # картинка на всеки 7-8 процеса -> създавам картинката в __main__ частта
             return True, (i, j)
     return False, (i, j)
 
 
+# TODO: да добавя map-ване към картинка с шарка за небесната сфера
+# TODO: да добавя графики на траекторията на няколко лъча и графики на измененинето на запазващите се величини
 if __name__ == "__main__":
     image = Image.new("RGB", (res, res), "white")
     draw = ImageDraw.Draw(image)
@@ -110,15 +155,21 @@ if __name__ == "__main__":
 
     start = time.time()
 
-    with concurrent.futures.ProcessPoolExecutor() as executor:
+    with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
         for result in executor.map(solve_ivp, i, j):
             fallen, coords = result
 
+            a, b = coords
             if fallen:
-                a, b = coords
-                pixels[a, b] = (0, 0, 0)
+                pixels[b, res-1-a] = (0, 0, 0)
+            else:
+                pixels[b, res-1-a] = (255, 255, 255)
 
     end = time.time()
-    print(f"Time taken: {end - start}")
+    elapsed_time_sec = end - start
+    hours, remainder = divmod(elapsed_time_sec, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    print(f"Time taken: {hours}:{minutes}:{seconds}")
 
-    image.save("miniBHtest.jpg")
+    image = image.transpose(Image.FLIP_LEFT_RIGHT)
+    image.save("miniBHtest.png")
