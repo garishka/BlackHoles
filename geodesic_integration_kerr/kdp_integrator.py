9ccd1de329207a786ab50c49df1cedc68c96c468
garishka <gergana.daskalova01@gmail.com>
2024-01-22 00:37:33 +0200

    Сега може да се добавят много параметри и то се опитва да махне тези, които отсъстват в сигнатурата на функцията за интегриране. ┐(￣ヮ￣)┌ (май работи)

--- geodesic_integration_kerr/kdp_integrator.py
+++ geodesic_integration_kerr/kdp_integrator.py
@@ -1,4 +1,7 @@
 import numpy as np
+from typing import Callable, Union, List
+import inspect
+import warnings
 
 a = np.asarray([[0, 0, 0, 0, 0, 0, 0],
                 [1 / 5, 0, 0, 0, 0, 0, 0],
@@ -16,7 +19,16 @@ b5 = np.asarray([5179 / 57600, 0, 7571 / 16695, 393 / 640, -92097 / 339200, 187 
 
 # func(t, qp, *params)
 # init = (q0, p0)
-def kdp45(func, init, t_init, h_init, num_iter, params):
+def kdp45(func: Callable, init: Union[np.ndarray, List], t_init: float, h_init: float, num_iter: int, **params):
+
+    if len(params) > (len(inspect.signature(func).parameters)-2):       # -2 за компенсиране на t, y
+        warnings.warn("The number of parameters given exceeds the number of positional arguments. "
+                      "The excess will be removed.")
+        params = {key: value for key, value in params.items() if key in inspect.signature(func).parameters.keys()}
+        if len(params) < (len(inspect.signature(func).parameters)-2):
+            raise TypeError("Check your parameter names. Something is wrong.")
+
+
     t = np.zeros(shape=num_iter)
     y = np.zeros(shape=(num_iter, 2*len(init)))
     t[0] = t_init
@@ -28,7 +40,7 @@ def kdp45(func, init, t_init, h_init, num_iter, params):
     for j in range(1, num_iter):
         t[j] = t[j-1] + h
         for i in range(1, 7):
-            k[i] = h * func(t[j-1] + h * c[i], y[j-1, :len(init)] + a[i] @ k, params)
+            k[i] = h * func(t[j-1] + h * c[i], y[j-1, :len(init)] + a[i] @ k, *params.values())
 
         y[j, :len(init)] = y[j-1, :len(init)] + b4 @ k
         y[j, len(init):] = y[j-1, :len(init)] + b5 @ k
--- geodesic_integration_kerr/test_files/kdp_primercheta.py
+++ geodesic_integration_kerr/test_files/kdp_primercheta.py
@@ -1,7 +1,7 @@
 from geodesic_integration_kerr.kdp_integrator import kdp45
 from matplotlib import pyplot as plt
 import numpy as np
-
+import inspect
 
 def exponential_decay(t, y, d):
     return - d * y
@@ -21,6 +21,12 @@ def periodic_uwu(t, y, d):
     return d * np.sin(d * t)
 
 
+# може да се използва за проверка колко параметъра се пускат на функцията за интегриране
+print(len(inspect.signature(periodic_uwu).parameters))
+# -> 3
+print(inspect.signature(kdp45))
+# -> (func: Callable, init: Union[numpy.ndarray, List], t_init: float, h_init: float, num_iter: int, **params)
+
 t_per, y_per = kdp45(func=periodic_uwu,
                      init=[.8],
                      t_init=0,
@@ -32,8 +38,8 @@ t = np.linspace(t_per[0], t_per[-1], 200)
 anl_per = - np.cos(5 * t) + 1.8
 
 
-def polynomial(t, y, d):
-    return d * t ** 3 + d / 2 * t ** 2 - t
+def polynomial(t, y, d, e):
+    return d * t ** 3 + e / 2 * t ** 2 - t
 
 
 t_poly, y_poly = kdp45(func=polynomial,
@@ -41,11 +47,13 @@ t_poly, y_poly = kdp45(func=polynomial,
                        t_init=4.,
                        h_init=0.1,
                        num_iter=500,
-                       params=2.3)
+                       e=2.3,
+                       d=2.4,       # останалите параметри ще бъдат махнати
+                       g=9.8,
+                       h=6.63)
 
 
 anl_poly = 2.3 / 4 * t_poly ** 4 + 2.3 / 6 * t_poly ** 3 - 0.5 * t_poly ** 2 - 159.733333333
-print(anl_poly)
 
 fig, ax = plt.subplots()
 ax.scatter(t_poly, y_poly[0], color="red")
