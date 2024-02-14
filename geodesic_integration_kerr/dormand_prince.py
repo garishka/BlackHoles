3a95bf519540f6055e4cc17f6b9b1dc25869daed
garishka <gergana.daskalova01@gmail.com>
2024-02-14 04:28:46 +0200

    added trajectory variable, to determine if the whole array with results is returned or just its last element; might have to come to that later
    + added some comments

--- geodesic_integration_kerr/dormand_prince.py
+++ geodesic_integration_kerr/dormand_prince.py
@@ -22,37 +22,28 @@ b5 = np.asarray([5179 / 57600, 0, 7571 / 16695, 393 / 640, -92097 / 339200, 187 
 
 ################################### ACCURACY AND ERRORS ################################################################
 
-RK45_ACCURACY = 1e-5
-EPSILON = 1e-16
-NUM_ITER = 10 ** 3
-DELTA = 1e-4
+RK45_ACCURACY = 1e-8        # Desired accuracy for the RK45 method
+EPSILON = 1e-16     # A small value to prevent division by zero errors
+NUM_ITER = 10 ** 3      # Maximum number of iterations allowed before termination
+DELTA = 1e-3        # Delta value used for evaluating if the photon trajectory originates from the black hole
 
 
 @dataclass
 class StepErrors:
+    # https://doi.org/10.48550/arXiv.1806.0869
     cur_err: float
     prev_err: float
     sec_prev_err: float
 
 
-err = StepErrors(RK45_ACCURACY, RK45_ACCURACY, RK45_ACCURACY)  # това изглежда мега тъпо като решение
-
-############################################# BH shit ##################################################################
-
-
-def b_values(alpha):
-    r1 = 2 * (1 + np.cos(2 * np.arccos(-alpha) / 3))
-    r2 = 2 * (1 + np.cos(2 * np.arccos(alpha) / 3))
-    b1 = - (r1 ** 3 - 3 * r1 ** 2 + alpha ** 2 * r1 + alpha ** 2) / (alpha * (r1 - 1))
-    b2 = - (r2 ** 3 - 3 * r2 ** 2 + alpha ** 2 * r2 + alpha ** 2) / (alpha * (r2 - 1))
-    return b1, b2, max(abs(r1), abs(r2))
-
+err = StepErrors(RK45_ACCURACY, RK45_ACCURACY, RK45_ACCURACY)
 
 #################################### DORMAND-PRINCE, uwu ###############################################################
 
+
 # func(t, qp, *params)
 # init = (q0, p0)
-def RK45(func: Callable, init: Union[np.ndarray, List], t_interval: Union[List, tuple], h_init: float, BH: bool,
+def RK45_mod(func: Callable, init: Union[np.ndarray, List], t_interval: Union[List, tuple], h_init: float, trajectory: bool,
          r_plus: float, **params) -> tuple:
 
     if len(params) > (len(inspect.signature(func).parameters)-2):       # -2 за компенсиране на t, y
@@ -64,7 +55,6 @@ def RK45(func: Callable, init: Union[np.ndarray, List], t_interval: Union[List, 
 
     # щото ми омръзна от input variables
     alpha = np.sqrt(1 - (r_plus-1) ** 2)
-    b1, b2, r = b_values(alpha)
 
     rev = bool((t_interval[0] > t_interval[-1]) or (t_interval[0] < 0))
 
@@ -82,7 +72,6 @@ def RK45(func: Callable, init: Union[np.ndarray, List], t_interval: Union[List, 
 
     k = np.zeros(shape=(7, len(init)), dtype=float)
 
-    current_r = list()
     j = 1
     while j < NUM_ITER:       # xrange is faster and uses less memory
         for i in range(7):
@@ -104,34 +93,22 @@ def RK45(func: Callable, init: Union[np.ndarray, List], t_interval: Union[List, 
             h *= (RK45_ACCURACY / (err.cur_err + EPSILON)) ** (0.58 / 5)
             h *= (RK45_ACCURACY / (err.prev_err + EPSILON)) ** (-0.21 / 5)
             h *= (RK45_ACCURACY / (err.sec_prev_err + EPSILON)) ** (0.10 / 5)
-            current_r.append(y[j, 1])
 
-            # не ми харесва колко if-ове има и повтарящ се код
-            #if BH and (b1 < y[j, 7] < b2):     # radial component < 40
-            #    if (y[0, 5] < 0 and rev) or (y[0, 5] > 0 and not rev):      # p_r
-            #        y = y.transpose()[:len(init)]
-            #        y = y[:, :np.count_nonzero(y[1])]      # remove zero elements
-            #        return False, t, y      # not fallen
-            #    elif y[j, 1] < r_plus + DELTA:       # avoiding circular imports, uwu(?)
-            #        y = y.transpose()[:len(init)]
-            #        y = y[:, :np.count_nonzero(y[1])]
-            #        return True, t, y
-            #elif BH and not (b1 < y[j, 7] < b2):
-            #    if y[j, 1] < r:
-            #        y = y.transpose()[:len(init)]
-            #        y = y[:, np.count_nonzero(y[1])]
-            #        return True, t, y
             if y[j, 1] < r_plus + DELTA:
                 y = y.transpose()[:len(init)]
                 y = y[:, :np.count_nonzero(y[1])]
-                return True, t, y
+                if trajectory:
+                    return True, t, y
+                else:
+                    return True, y[:, -1]
 
             j += 1
+
         else:
-            # като няма достатъчно точност искаме по-добра стъпка, не дубликати на предните резултати???????
-            # t[j] = t[j-1]
-            # y[j, :] = y[j-1, :]
             h *= 0.8 * (RK45_ACCURACY / (err.cur_err + 1e-10)) ** 0.25
 
     y = y.transpose()[:len(init)]
-    return 0, t, y
+    if trajectory:
+        return False, t, y
+    else:
+        return False, y[:, -1]
